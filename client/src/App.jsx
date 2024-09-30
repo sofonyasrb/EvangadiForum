6f92fe64dc66a59a498f18ca0e9ee6745901bfc9
Ephraimdebel <ephraimdebel@gmail.com>
2024-09-30 09:48:59 -0700

    initial commit and vite initialized

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      Hey Group 1
+      <br />
+      don not forget to create your own branch .<br />
+      becareful on your folder structure .<br />
+      before you commit check the syntax is correct and free from error . <br/>
+      you can reach out to me if you have any problem .
+      <br /><br />
+          Ephraim Debel
+    </div>
+  )
+}
+
+export default App
