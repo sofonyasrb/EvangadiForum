3d13b40a6cb8a3171c5eb0fddbdaef8f94c81749
Bethlehem Nigussie <bethlehemnigussie19@gmail.com>
2024-10-06 12:49:10 -0700

    addding the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import { Route,Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div>
-      Hey Group 1
-      <br />
-      don not forget to create your own branch .<br />
-      becareful on your folder structure .<br />
-      before you commit check the syntax is correct and free from error . <br />
-      you can reach out to me if you have any problem .
-      <br />
-      <br />
-      Ephraim Debel
-      <h1>Welcome to first pull request</h1>
-      <h1>wellcome to second pull request</h1>
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        
+      </Routes> */}
+      <Footer/>
     </div>
   );
 }
