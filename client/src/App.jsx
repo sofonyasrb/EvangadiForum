7253bcb23805f0104401213de478f0681750068b
Fekadelika <fekade.wolde@yahoo.com>
2024-10-07 22:53:46 -0400

    Fekade done this

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,11 @@
 import React from "react";
+import AskQuestion from "./components/AskQuestion/AskQuestion";
 
 function App() {
   return (
-    <div>
-      Hey Group 1
-      <br />
-      don not forget to create your own branch .<br />
-      becareful on your folder structure .<br />
-      before you commit check the syntax is correct and free from error . <br />
-      you can reach out to me if you have any problem .
-      <br />
-      <br />
-      Ephraim Debel
-      <h1>Welcome to first pull request</h1>
-      <h1>wellcome to second pull request</h1>
-    </div>
+    <>
+   <AskQuestion/>
+    </>
   );
 }
 
