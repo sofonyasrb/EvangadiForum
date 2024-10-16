2aa7e99956c34bac3d6077cb7c36d729c2d20269
JiruGutema <jethior1@gmail.com>
2024-10-16 14:40:12 +0300

    import updated from controller to Controller

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,31 +1,27 @@
-
-
 const express = require("express");
 const router = express.Router();
 const dbConnection = require("../db/dbConfig");
-const bcrypt = require('bcrypt');
-const { StatusCodes } = require('http-status-codes');
-const {login, checkUser} = require('../controller/usercontroller');
-const authMiddleware = require('../middleWare/authMiddleWare');  
-
+const bcrypt = require("bcrypt");
+const { StatusCodes } = require("http-status-codes");
+const { login, checkUser } = require("../Controller/usercontroller");
+const authMiddleware = require("../middleWare/authMiddleWare");
 
 // Register route
-router.post('/register', register);
+router.post("/register", register);
 
 //login
-router.post("/login",login)
- 
-    //check user
+router.post("/login", login);
 
-router.get("/check",authMiddleware, checkUser)
- 
+//check user
+
+router.get("/check", authMiddleware, checkUser);
 
 // Controller
 async function register(req, res) {
   const { username, firstname, lastname, email, password } = req.body;
   if (!email || !password || !firstname || !lastname || !username) {
-    console.log("here")
-    return res.status(400).json({ message: 'All fields are required' });
+    console.log("here");
+    return res.status(400).json({ message: "All fields are required" });
   }
 
   try {
@@ -36,14 +32,18 @@ async function register(req, res) {
     );
 
     if (user && user.length > 0) {
-      console.log("here2")
+      console.log("here2");
 
-      return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Email or username already registered' });
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "Email or username already registered" });
     }
 
     // Check if the password length is valid
     if (password.length < 8) {
-      return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Password must be at least 7 characters' });
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "Password must be at least 7 characters" });
     }
 
     // Encrypt the password
@@ -56,13 +56,16 @@ async function register(req, res) {
       [username, firstname, lastname, email, hashedPassword]
     );
 
-    return res.status(StatusCodes.CREATED).json({ message: 'User created successfully' });
+    return res
+      .status(StatusCodes.CREATED)
+      .json({ message: "User created successfully" });
   } catch (error) {
     console.error(error.message);
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Something went wrong, please try again later" });
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Something went wrong, please try again later" });
   }
 }
 
 // Export the router and controller functions
 module.exports = router;
-
