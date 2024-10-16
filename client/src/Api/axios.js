491e141a760ccbc030f098eef78fe6537d3c5353
JiruGutema <jethior1@gmail.com>
2024-10-16 15:12:31 +0300

    API is integrated from render

--- client/src/Api/axios.js
+++ client/src/Api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: "http://localhost:5500/api/",
-    // baseURL: "https://evangadi-forum-backend-xi.vercel.app/api/",
-})
+  // baseURL: "http://localhost:5500/api/",
+  baseURL: "https://evangadiforum-yalv.onrender.com/api/",
+  // baseURL: "https://evangadi-forum-backend-xi.vercel.app/api/",
+});
 export default instance;
