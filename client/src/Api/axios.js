5fb63adfc284076811b1c835b76643e02d7bc213
sofonyasrb <92786649+sofonyasrb@users.noreply.github.com>
2024-10-17 00:47:59 -0600

    modified with new URL

--- client/src/Api/axios.js
+++ client/src/Api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
   // baseURL: "http://localhost:5500/api/",
-  baseURL: "https://evangadiforum-yalv.onrender.com/api/",
+  baseURL: "https://evangadiforum-y704.onrender.com/api/",
   // baseURL: "https://evangadi-forum-backend-xi.vercel.app/api/",
 });
 export default instance;
